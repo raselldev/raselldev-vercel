a4bc1134932eff1b95f81c81d33eec6c73a8b583
Raka Rasell <rrrraka@gmail.com>
2022-03-07 17:40:52 +0700

    up

---
+++ constants/data.js
@@ -0,0 +1,83 @@
+const userData = {
+  githubUsername: "raselldev",
+  name: "Raka Rasell",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpeg",
+  email: "rakarasell@outlook.com",
+  phone: "+62 8568692835",
+  address: "Jakarta, Indonesia.",
+  projects: [
+    {
+      title: "Netflix Clone",
+      link: "https://netflix-clone-cd386.web.app/",
+      imgUrl: "/netflix-clone.png",
+    },
+    {
+      title: "Facebook Clone",
+      link: "https://facebook-messenger-clone-95627.web.app/",
+      imgUrl: "/facebook-clone.png",
+    },
+    {
+      title: "Pokedex",
+      link: "http://pokedex-raselldev.vercel.app/",
+      imgUrl: "/pokedex.jpeg",
+    },
+    
+  ],
+  about: {
+    title:
+      "Hello my name is Raka Rasell. You can call me Raka or Rasell",
+    description: [
+      `I really enjoy writing code. The code I created for the first time was a website in 2015 using HTML, CSS. The first time I aspired to be a network engineer, but all that changed when I entered the world of lectures.`,
+      `In college I studied algorithms, data structures, mathematics, and a little introduction to programming languages. The programming language I used at that time was JAVA. JAVA is a language that I think is very difficult but now I really like JAVA.`,
+      `Skills/Interest: Java, PHP, Go, SQL, .NET, Linux, Mac, Virtualization, Styling Component (HTML, CSS, JS), Node, Raspberry.`,
+    ],
+    currentProject: "Honda Indonesia",
+    currentProjectUrl: "https://honda-indonesia.com",
+  },
+  experience: [
+    {
+      title: "IT Application & Development",
+      company: "PT. Honda Prospect Motor",
+      year: "2021 - Present",
+      companyLink: "https://honda-indonesia.com",
+      desc: "Create internal apps and integration data with Honda Global and another Genpo (Honda in another country).",
+    },
+    {
+      title: "Co-Founder",
+      company: "GIJOEFIX",
+      year: "2019 - 2021",
+      companyLink: "#",
+      desc: "Developed gadget home service in Jakarta Area",
+    },
+    {
+      title: "Frontend Web Developer",
+      company: "PT. Traders Family International",
+      year: "2019",
+      companyLink: "https://tradersfamily.co.id",
+      desc: "Developed landing page for Digital Marketing Campaign",
+    },
+    {
+      title: "Video Production Specialist",
+      company: "PT. Traders Family International",
+      year: "2017 - 2019",
+      companyLink: "https://tradersfamily.co.id",
+      desc: "Create videos, color grading, for Digital Marketing Campaign",
+    },
+    {
+      title: "Digital Marketing",
+      company: "PT. Traders Family International",
+      year: "2017",
+      companyLink: "https://tradersfamily.co.id",
+      desc: "As Digital Marketing, I create a social media plan, email marketing, and post feed.",
+    },
+  ],
+  resumeUrl:
+    "https://github.com/raselldev/certificate/blob/main/Resume-Raka-Rasell.pdf",
+  socialLinks: {
+    linkedin: "https://linkedin.com/in/rrsll",
+    github: "https://github.com/raselldev",
+  },
+};
+
+export default userData;
